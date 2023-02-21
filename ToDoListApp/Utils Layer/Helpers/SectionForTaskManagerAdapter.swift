b1a32c144d1cc8bc6dde2b3ede793269b9f1f7a3
sviridov <sviridov@bifit.com>
2023-02-21 23:37:10 +0300

    add description for SectionForTaskManagerAdapter

--- ToDoListApp/Utils Layer/Helpers/SectionForTaskManagerAdapter.swift
+++ ToDoListApp/Utils Layer/Helpers/SectionForTaskManagerAdapter.swift
@@ -16,15 +16,18 @@ final class SectionForTaskManagerAdapter: ISectionForTaskManagerAdapter {
 		self.taskManager = taskManager
 	}
 	
+	/// Возвращает массив с секциями
 	public func getSections() -> [Section] {
 		sections
 	}
 	
+	/// Получить задачи для определнной секции
 	public func getSection(forIndex index: Int) -> Section {
 		let i = min(index, sections.count - 1)
 		return sections[i]
 	}
 	
+	/// Получить секцию по индексу
 	public func getTasksForSection(section: Section) -> [Task] {
 		switch section {
 		case .completed:
